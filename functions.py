ad6b3bfd9bad96e770435df623a22a3bf57b6ffd
Andrew Denta <andrew.denta@gmail.com>
2016-04-17 20:08:46 -0400

    passed initial test cases

--- functions.py
+++ functions.py
@@ -2,10 +2,20 @@ import csv
 
 parties = ['D','R']
 
+def cleanNumber(numString):
+
+    if "." in numString:
+        return int(numString[:numString.index(".")])
+    else:
+        try:
+            return int(numString)
+        except ValueError:
+            return -1
+
 # for a given year and state, return a dictionary of all the districts, and what that district voted for.
 def getDistricts( state, fnYear):
     for i in range(100):
-        district =  getDistrict(state,fnYear,str(i).zfill(2))
+        district =  getDistrict(state,fnYear,i)
         if district == {} and i is not 0:
             break
         print district
@@ -14,11 +24,6 @@ def getDistricts( state, fnYear):
 # for a given state district, for a given year, return a dictionary that tells how many democratic votes were cast,
 # and how many republican votes were cast, and how many other votes were cast.
 def getDistrict(state,fnYear, number):
-    try:
-         open(fnYear)
-    except IOError:
-        print "bad number:",number
-        return {}
     with open(fnYear) as yearTable:
         district = {}
         districts = csv.DictReader(yearTable)
@@ -32,11 +37,11 @@ def getDistrict(state,fnYear, number):
                 votes = partialDistrict['GENERAL ']
             except KeyError:
                 votes = partialDistrict['GENERAL RESULTS']
-            isDataClean = votes is not "" and partialDistrict['PARTY'] is not ""
-            matchingConditions = stateKey.lower() == state.lower() and partialDistrict['DISTRICT'] == number
 
-            if "2000" in fnYear:
-                print isDataClean,":",matchingConditions
+            partialDistrictNumber = cleanNumber(partialDistrict['DISTRICT'])
+            isDataClean = votes is not "" and partialDistrict['PARTY'] is not ""
+            matchingConditions = stateKey.lower() == state.lower() and partialDistrictNumber == number
+
             if isDataClean and matchingConditions:
 
                 district[partialDistrict['PARTY'][:1]] = votes
--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
 import functions
 
-assert functions.getDistrict("AL","./tables/2010.csv","01")['R'] == '129063.0', "problem with rep count"
-assert functions.getDistrict("AZ","./tables/2010.csv","01")['D'] == '99233.0', "problem with dem count"
-assert functions.getDistrict("AZ","./tables/2010.csv","101")== {}, "problem with edge cases"
-assert functions.getDistrict("AZ","./tables/2002.csv","02")['R'] == '100359.0',"problem reading from 2002"
+assert functions.getDistrict("AL","./tables/2010.csv",1)['R'] == '129063.0', "problem with rep count"
+assert functions.getDistrict("AZ","./tables/2010.csv",1)['D'] == '99233.0', "problem with dem count"
+assert functions.getDistrict("AZ","./tables/2010.csv",101)== {}, "problem with edge cases"
+assert functions.getDistrict("AZ","./tables/2002.csv",2)['R'] == '100359.0',"problem reading from 2002"
+assert functions.getDistrict("AL","./tables/2000.csv",2)['D'] == '64958.0',"problem reading from 2000"
 
-print  functions.getDistrict("AL","./tables/2000.csv","02")
-# assert functions.getDistrict("AL","./tables/2000.csv","02")['D'] == '64958.0',"problem reading from 2000"
+
+functions.getDistricts("AZ","./tables/2010.csv")
